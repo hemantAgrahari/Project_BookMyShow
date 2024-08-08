a6951f2e2138743b377553af9601df16c6fbeff4
Hemant <Hemant11102219@gmail.com>
2024-08-08 15:20:13 +0530

    bookingsystem

---
+++ server/config/dbConfig.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+mongoose.set('strictQuery', false);
+
+const connectToDatabase = async() => {
+    try {
+        const {connection} = await mongoose.connect(
+            `mongodb+srv://hemant11102219:${process.env.DATABASE_PASSWORD}@cluster0.ojlnq2k.mongodb.net/bookmyshow?retryWrites=true&w=majority&appName=Cluster0`
+        );
+
+        console.log(connection);
+    
+        if (connection) {
+            console.log(`Connected to databse ${connection.host}`);
+        }
+
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+export default connectToDatabase;
